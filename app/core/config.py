30a3b3255784397069361a4e7548c01f116b0160
201RichK <amosamospascal@yahoo.com>
2023-10-02 10:56:46 +0000

    migrations with alembic migrate to head

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,11 @@ from functools import lru_cache
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
     TITLE: str = "FASTAPI-BOILERPLATE"
     ENV: str = "dev"
